0513c101acd547cb7dc622f37f34e963fd99366c
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2021-10-25 14:23:29 +0200

    Add new style

--- src/ILICheck.Web/ClientApp/src/Layout.js
+++ src/ILICheck.Web/ClientApp/src/Layout.js
@@ -1,5 +1,8 @@
 import './App.css';
 import React from 'react';
+import geow_logo from './img/GeoW.png'
+import github_logo from './img/GitHub-Mark-Light-64px.png'
+import qgis_logo from './img/qgis_transparent.png'
 import Home from './Home';
 
 export const Layout = props => {
@@ -8,6 +11,12 @@ export const Layout = props => {
   return (
     <div className="app">
       <header className="header-style">
+        <div className="icon">
+          <a href="https://www.geowerkstatt.ch" title="www.geowerkstatt.ch" target="_blank" rel="noreferrer">
+            <img src={geow_logo} width="150" alt="GeoWerkstatt_Logo" />
+          </a>
+        </div>
+        <div className="subtitle">ilicop - online <a href="https://www.interlis.ch/downloads/ilivalidator" title="Zum ilivalidator" target="_blank" rel="noreferrer">Ilivalidator</a></div>
       </header>
       <main>
         <Home connection={connection}
@@ -21,7 +30,18 @@ export const Layout = props => {
               setUploadLogsEnabled={setUploadLogsEnabled} />
       </main>
       <footer className="footer-style">
+        <div className="flex-item"></div>
+        <div className="flex-item"></div>
+        <div className="flex-item"></div>
+        <div className="flex-icons">
+          <a href="https://github.com/GeoWerkstatt/interlis-check-service" title="Link zum github reporsitory" target="_blank" rel="noreferrer">
+            <img className="icon" src={github_logo} color="#c1c1c1" width="40" alt="Github_Logo" />
+          </a>
+          <a href="https://plugins.qgis.org/plugins/xtflog_checker/" title="Link zum QGIS Plugin XTFLog Checker" target="_blank" rel="noreferrer">
+            <img className="icon" src={qgis_logo} color="#c1c1c1" width="40" alt="QGIS_Logo" />
+          </a>
           <span className="version-tag">{process.env.REACT_APP_VERSION ? 'v' + process.env.REACT_APP_VERSION + '+' : ''}{process.env.REACT_APP_REVISION ?? process.env.NODE_ENV}</span>
+        </div>
       </footer>
     </div>
   );
