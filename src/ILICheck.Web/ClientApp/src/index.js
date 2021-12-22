23486083db92874996890518811547006c4c98fc
Oliver Gut <oliver.gut@geowerkstatt.ch>
2021-12-22 07:43:36 +0100

    Re-enable Reacts strict mode
    Strict mode highlights potential problems in development mode only.

--- src/ILICheck.Web/ClientApp/src/index.js
+++ src/ILICheck.Web/ClientApp/src/index.js
@@ -4,4 +4,4 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
