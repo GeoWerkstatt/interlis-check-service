f614e5ce28f826325f8c57f0c23433e0918edc20
Oliver Gut <oliver.gut@geowerkstatt.ch>
2022-01-21 08:56:28 +0100

    Improve code readability

--- src/ILICheck.Web/ClientApp/src/app.js
+++ src/ILICheck.Web/ClientApp/src/app.js
@@ -15,10 +15,8 @@ function App() {
   const updateLog = useCallback((message, { disableUploadLogs = true } = {}) => {
     if (disableUploadLogs) setUploadLogsEnabled(false);
     setLog(log => {
-       if (message === log[log.length -1]) 
-       { return log} 
-       else 
-      { return [...log, message]}
+       if (message === log[log.length -1]) return log
+       else return [...log, message]
     });
   }, []);
 
