585eb0670977c0e73230e2aad685df09b7038267
Oliver Gut <oliver.gut@geowerkstatt.ch>
2022-02-21 14:31:53 +0100

    Add tests

---
+++ tests/ILICheck.Web.Test/ExtensionsTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace ILICheck.Web
+{
+    [TestClass]
+    public class ExtensionsTest
+    {
+        [TestMethod]
+        public void CleanupGpkgModelNames()
+        {
+            var configItems = new Dictionary<string, string>
+            {
+                ["Validation:BlacklistedGpkgModels"] = "VIOLENTGLEE;CALENDARSEAGULL",
+            };
+
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(configItems)
+                .Build();
+
+            var result = new[]
+            {
+                "ANGRYCHEF IRATENIGHT { REDTOTE Product Data} MAESTROTRAWL{ PEEVEDSCAN} VIOLENTGLEE {INTERLIS }",
+                "SLEEPYGOPHER",
+                "CALENDARSEAGULL {REV4}",
+                " AUTOMAESTRO {VIOLETCALENDAR } ",
+                "VIOLENTTOTE VIOLENTTOTE",
+            }.CleanupGpkgModelNames(configuration);
+
+            Assert.AreEqual("ANGRYCHEF;IRATENIGHT;MAESTROTRAWL;SLEEPYGOPHER;AUTOMAESTRO;VIOLENTTOTE", result);
+        }
+    }
+}
