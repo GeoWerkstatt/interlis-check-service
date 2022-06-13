c3f4cadcbb126b46e376cef4065fc8772c265f1a
Oliver Gut <oliver.gut@geowerkstatt.ch>
2022-06-13 13:36:37 +0200

    Add missing response code

--- src/ILICheck.Web/Controllers/UploadController.cs
+++ src/ILICheck.Web/Controllers/UploadController.cs
@@ -76,6 +76,7 @@ namespace ILICheck.Web.Controllers
         [HttpPost]
         [SwaggerResponse(StatusCodes.Status201Created, "The validation job was successfully created and is now scheduled for execution.", typeof(UploadResponse), new[] { "application/json" })]
         [SwaggerResponse(StatusCodes.Status400BadRequest, "The server cannot process the request due to invalid or malformed request.", typeof(ProblemDetails), new[] { "application/json" })]
+        [SwaggerResponse(StatusCodes.Status413PayloadTooLarge, "The transfer file is too large. Max allowed request body size is 200 MB.")]
         [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1629:DocumentationTextMustEndWithAPeriod", Justification = "Not applicable for code examples.")]
         [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1028:CodeMustNotContainTrailingWhitespace", Justification = "Not applicable for code examples.")]
         public async Task<IActionResult> UploadAsync(ApiVersion version, IFormFile file)
