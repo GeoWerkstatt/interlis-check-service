4929508bbfb44b3f9e090a5445cd211ad746cf49
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2025-04-22 15:26:02 +0200

    Fix modal scrolling

--- src/ILICheck.Web/ClientApp/src/modalContent.jsx
+++ src/ILICheck.Web/ClientApp/src/modalContent.jsx
@@ -6,15 +6,17 @@ export const ModalContent = (props) => {
 
   return (
     <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
-      <Modal.Body>
-        {type === "markdown" && <ReactMarkdown linkTarget="_blank" children={content || ""} />}
-        {type === "raw" && content}
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="outline-dark" onClick={props.onHide}>
-          Schliessen
-        </Button>
-      </Modal.Footer>
+      <div style={{ maxHeight: "calc(100vh - 58px)", display: "flex", flexDirection: "column" }}>
+        <Modal.Body style={{ flex: 1, overflowY: "auto" }}>
+          {type === "markdown" && <ReactMarkdown linkTarget="_blank" children={content || ""} />}
+          {type === "raw" && content}
+        </Modal.Body>
+        <Modal.Footer style={{ flex: "0 0 auto" }}>
+          <Button variant="outline-dark" onClick={props.onHide}>
+            Schliessen
+          </Button>
+        </Modal.Footer>
+      </div>
     </Modal>
   );
 };
